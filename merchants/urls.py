7fd9f8d7061e2780a4a44a8c7c13ba1196960e86
Padmanabhan <padmanabhansridhar2005@gmail.com>
2026-04-26 01:47:31 +0530

    Added endpoint 'api/v1/merchants/' with necessary config file changes, tested, ran migrations for django.contrib.auth dependency

--- merchants/urls.py
+++ merchants/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.merchant_list, name="merchant-list")
+]
--- merchants/views.py
+++ merchants/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import status   
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Merchant
+from .serializers import MerchantSerializer
+from ledger.models import LedgerEntry
+from ledger.serializers import LedgerEntrySerializer
+
+@api_view(["GET"])
+def merchant_list(request):
+    merchants = Merchant.objects.prefetch_related("bank_accounts").all()
+    serializer = MerchantSerializer(merchants, many=True)
+    return Response(serializer.data)
+
+
+
