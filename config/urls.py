c30d623c713e67a2e5db54a16e32291031ada021
Padmanabhan <padmanabhansridhar2005@gmail.com>
2026-04-27 01:25:02 +0530

    ran DB migrations after fixes

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("api/v1/merchants/", include("merchants.urls")),
-    path("api/ledger/", include("ledger.urls")),
     path("api/v1/payouts/", include("payouts.urls")),
 ]
